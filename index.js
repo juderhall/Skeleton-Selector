3f619a27c2c24956815a09e366d4463b24822c10
ikhall <kimballh2020@gmail.com>
2022-05-16 19:42:05 -0600

    Create HTML Interface Complete

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ let incrementButton = document.querySelector("#incrementButton")
 console.log(incrementButton)
 
 incrementButton.addEventListener('click', () => {
-    console.log("This button is working!")
+    console.log("Increment Button is Clickable")
 })
+
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
 
+const skelliesNum = 0
+const decrementMode = false
+
 app.listen(4000, () => console.log("App running on port 4000"))
 
