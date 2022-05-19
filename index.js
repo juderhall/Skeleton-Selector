2730f149b871f701e8662e0019e8b71b38edc862
ikhall <kimballh2020@gmail.com>
2022-05-19 11:50:40 -0600

    Frontend & Backend GET Call in Progress

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
+
 let incrementButton = document.querySelector("#incrementButton")
 
 incrementValue = 1
 
 incrementButton.addEventListener('click', () => {
-    axios.put("http://localhost:4000/api/skelliesNum", incrementValue)       
+    axios.put("http://localhost:4000/api/skelliesNum", incrementValue) 
+    axios.get("http://localhost:4000/api/skelliesNum")
+        .then(function (response) {
+            
+        })      
 })
 
--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.text({type:"*/*"}));
 
-let skelliesNum = 0
+let skelliesNum = [0]
 let decrementMode = false
 
 app.put("/api/skelliesNum", (req, res) => {
-    console.log(req.body)
     let incrementValue = req.body
-    console.log(incrementValue)
     incrementValue = parseInt(incrementValue)
-    console.log(incrementValue)
     skelliesNum = skelliesNum + incrementValue
-    console.log(skelliesNum)
     res.sendStatus(200)
+})
+
+//cannot figure out how to send data
+app.get("/api/skelliesNum", (req, res) => {
 })
 
 app.listen(4000, () => console.log("App running on port 4000"))
