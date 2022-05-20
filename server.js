cc5b320bad3f57a3598313f4e19458b5f253b57d
ikhall <kimballh2020@gmail.com>
2022-05-19 21:57:11 -0600

    Basic Decrement In Progress

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(bodyParser.text({type:"*/*"}));
 let skelliesNum = 0
 let decrementMode = false
 
+app.get("/api/skelliesNum", (req, res) => {
+    res.status(200).send({skelliesNum})
+})
+
 app.put("/api/skelliesNum", (req, res) => {
     let incrementValue = req.body
     incrementValue = parseInt(incrementValue)
@@ -15,10 +19,16 @@ app.put("/api/skelliesNum", (req, res) => {
     res.sendStatus(200)
 })
 
-//cannot figure out how to send data
-app.get("/api/skelliesNum", (req, res) => {
-    res.send({skelliesNum})
+app.get("/api/decrementMode", (req, res) => {
+    console.log(decrementMode)
+    res.status(200).send({decrementMode})
 })
+
+app.put("/api/decrementMode", (req, res) => {
+    let decrementMode = red.body
+})
+
+
 
 app.listen(4000, () => console.log("App running on port 4000"))
 
